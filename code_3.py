047012748dfad7f38616bd21d15a55498c4cc64e
Jack <143327277+Jack30046@users.noreply.github.com>
2023-12-31 18:09:06 +0000

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,26 @@
+def generate_random_data():
+    data = [random.randint(1, 100) for _ in range(10)]
+def main():
+import random
+
+        print(f"Random Number: {item}")
+    data = generate_random_data()
+    main()
+
+
+
+    return data
+    for item in data:
+if __name__ == "__main__":def generate_random_data():
+
+    for item in data:
+    main()
+
+    return data
+def main():
+import random
+
+    data = [random.randint(1, 100) for _ in range(10)]
+    data = generate_random_data()
+        print(f"Random Number: {item}")
+if __name__ == "__main__":
